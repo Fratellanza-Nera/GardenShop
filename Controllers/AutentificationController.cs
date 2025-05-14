e55b8767c89c25dd9254fddf2887f8fa0b71971b
Dinu <dinu.vitu@isa.utm.md>
2025-05-14 23:10:02 +0300

    Add Login/Register and Categories

--- Controllers/AutentificationController.cs
+++ Controllers/AutentificationController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc;
 
 namespace GardenShop.Controllers
 {
     public class AutentificationController : Controller
     {
-        // GET: Autentification
-        public ActionResult Index()
+        // GET: /Autentification/Login
+        public ActionResult Login()
         {
             return View();
+        }
+
+        // GET: /Autentification/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Autentification/Login
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Здесь логика авторизации
+            if (username == "admin" && password == "admin")
+                return RedirectToAction("Index", "Home");
+
+            ViewBag.Error = "Invalid credentials";
+            return View();
+        }
+
+        // POST: /Autentification/Register
+        [HttpPost]
+        public ActionResult Register(string username, string password)
+        {
+            // Здесь логика регистрации (например, сохранить в БД)
+            ViewBag.Message = "Account created!";
+            return RedirectToAction("Login");
         }
     }
 }
---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using GardenShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GardenShop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // Симуляция БД (временный список)
+        private static List<Product> AllProducts = new List<Product>
+        {
+            new Product { Id = 1, Name = "Apple Tree", Category = "Trees", Description = "Green apple tree", ImageUrl = "/img/AppleTree.png", Price = 25 },
+            new Product { Id = 2, Name = "Cherry Blossom", Category = "Trees", Description = "Beautiful cherry blossom", ImageUrl = "/img/Cherry Blossom.png", Price = 30 },
+            new Product { Id = 3, Name = "Rose", Category = "Flowers", Description = "Red rose", ImageUrl = "/img/Rose.png", Price = 10 },
+            new Product { Id = 4, Name = "Shovel", Category = "Tools", Description = "Metal shovel", ImageUrl = "/img/Shovel.png", Price = 15 }
+        };
+
+        public ActionResult Index(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return RedirectToAction("Home", "Home");
+            }
+
+            var products = AllProducts
+                .Where(p => p.Category != null && p.Category.ToLower() == name.ToLower())
+                .ToList();
+
+            ViewBag.CategoryName = name;
+            return View(products);
+        }
+    }
+}
