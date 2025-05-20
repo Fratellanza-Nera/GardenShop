3e8f530525f85ef2d33f53564ded1b5828001195
Nicolae-Bivol <nicolae.bivol@isa.utm.md>
2025-05-20 12:28:24 +0300

    setting account controler

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GardenShop.Services;
+
 
 namespace GardenShop.Controllers
 {
-    public class AccountController : Controller
-    {
-        // GET: Account
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+     public class AccountController : Controller
+     {
+          private readonly UserService _userService = new UserService();
+
+          public ActionResult Register() => View();
+
+          [HttpPost]
+          public ActionResult Register(string username, string password)
+          {
+               if (_userService.Register(username, password))
+               {
+                    Session["Username"] = username;
+                    return RedirectToAction("Index", "Home");
+               }
+
+               ViewBag.Error = "Username already exists!";
+               return View();
+          }
+
+          public ActionResult Login() => View();
+
+          [HttpPost]
+          public ActionResult Login(string username, string password)
+          {
+               if (_userService.Login(username, password))
+               {
+                    Session["Username"] = username;
+                    return RedirectToAction("Index", "Home");
+               }
+
+               ViewBag.Error = "Invalid username or password.";
+               return View();
+          }
+
+          public ActionResult Logout()
+          {
+               Session.Clear();
+               return RedirectToAction("Index", "Home");
+          }
+     }
 }
